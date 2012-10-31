6e98383a1179c34dda9f39028a174ef3150a18b1
Peter H <peterhalim@live.com>
2012-10-30 19:44:21 -0800

    Update bs variable

--- BASE.bat
+++ BASE.bat
@@ -3,7 +3,7 @@ SETLOCAL ENABLEEXTENSIONS
 SET e=echo
 SET p=pause
 CD /D "%~dp0"
-for /f %%A in ('"prompt $H & echo on & for %%B in (1) do rem"') do set "BS=%%A"
+for /f %%A in ('"prompt $H & echo on & for %%B in (1) do rem"') do set "bs=%%A"
 REM Settings --------
 :run
 cls
