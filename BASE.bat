d261fff6c114d4bf0823582ff90e4a31de6e1ab1
Peter H <peterhalim@live.com>
2012-11-05 22:03:56 -0800

    Added more feature & lowercased the commands

--- BASE.bat
+++ BASE.bat
@@ -1,16 +1,19 @@
 @echo off
-SETLOCAL ENABLEEXTENSIONS
-SET e=echo
-SET p=pause
-CD /D "%~dp0"
+setlocal enableextensions
+set e=echo
+set p=pause
+pushd "%cd%\"
+cd /D "%~dp0"
 for /f %%A in ('"prompt $H & echo on & for %%B in (1) do rem"') do set "bs=%%A"
-REM Settings --------
+rem pause>nul|set /P =%bs% Press any key to continue . . . 
+rem Settings --------
 :run
 cls
 %e%.
-%e%  Running...& %e%. & %p%
+%e%  Running...& %e%. & pause>nul|set /P =%bs% Press any key to continue . . . 
 :exit
 cls
 %e%.
-TITLE %SystemRoot%\system32\cmd.exe
+title %SystemRoot%\system32\cmd.exe
 %e%  Goodbye!
+popd
